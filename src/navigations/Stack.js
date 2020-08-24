4538132b1ed88260db367fffcb99df3d7af063b0
Unknown <hackurity01@gmail.com>
2020-08-25 00:15:30 +0900

    Change animation to vertical when modal showing

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import MainView from '../views/MainView';
 import AddMemoView from '../views/AddMemoView';
@@ -8,7 +8,10 @@ const Stack = createStackNavigator();
 
 function StackNavigation() {
 	return (
-		<Stack.Navigator screenOptions={{ gestureEnabled: false }} headerMode={'none'}>
+		<Stack.Navigator
+			screenOptions={{ gestureEnabled: false, cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS }}
+			headerMode={'none'}
+		>
 			<Stack.Screen name="Main" component={MainView} />
 			<Stack.Screen name="AddMemo" component={AddMemoView} />
 		</Stack.Navigator>
